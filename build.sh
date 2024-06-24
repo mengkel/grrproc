99af70852978ab5b404d22039289a02cc23bbad0
Bradley Meyer <mbradle@clemson.edu>
2024-06-24 10:29:27 -0400

    Update script.

--- build.sh
+++ build.sh
@@ -3,7 +3,25 @@
 rm -fr dist
 cd grrproc
 black --line-length=79 *.py
-cd ..
+pylint *.py
+cd ../.github/workflows/
+pytest
+cd ../..
+
 python -m pip install --upgrade build
 python -m build
 python -m pip install --upgrade twine
+
+echo ""
+echo "All version numbers must be the same:"
+echo ""
+
+grep version grrproc/__about__.py | grep -v ","
+grep version CITATION.cff | grep -v "cff-version"
+grep Version doc/source/changelog.rst | grep -v Versioning | head -1
+
+echo ""
+echo "Check the release date:"
+echo ""
+grep date CITATION.cff
+echo ""
